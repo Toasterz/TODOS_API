cb7766850513f000766f583f6eb0e88250e3f096
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-07-01 20:36:37 -0400

    set up put and post routes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require("express");
-var server = express();
+var bodyParser = require("body-parser");
+var lowdb = require("lowdb");
+var db = lowdb("db.json");
+var uuid = require("uuid");
 
+var server = express();
 var port = process.env.PORT || 8080;
+
+
+//database initialization
+db.defaults({todos: []})
+  .value();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
 
 server.get('/todos', function(request, response)
 {
@@ -15,7 +27,16 @@ server.get('/todos/:id', function(request, response)
 
 server.post('/todos', function(request, response)
 {
-  response.send('POST todos');
+  var todo = {
+    id: uuid.v4(),
+    description: request.body.description,
+    isComplete: false,
+  };
+  var result = db.get('todos')
+                .push(todo)
+                .last()
+                .value();
+  response.send(result);
 });
 
 server.put('/todos/:id', function(request, response)
